77f3a31735616e92ead8e588bea899e111d7fdea
drfalcoew <drfalcoew@gmail.com>
2018-10-08 20:57:49 -0700

    Added left and right buttons & more

--- Projects/CarGame/CarGame/GameViewController.cs
+++ Projects/CarGame/CarGame/GameViewController.cs
@@ -10,7 +10,7 @@ namespace CarGame
 {
     public partial class GameViewController : UIViewController {
 
-        // Initializing objects
+        // Initializing Scene objects
         SCNView gameView;
         SCNScene gameScene;
         SCNNode cameraNode;
@@ -20,6 +20,14 @@ namespace CarGame
         SCNNode floorNode;
         SCNBox userShape;
         SCNNode user;
+
+
+        // Initializing View objects
+
+        UIButton leftBtn;
+        UIButton rightBtn;
+
+
 
         protected GameViewController(IntPtr handle) : base(handle) {
             // Note: this .ctor should not contain any initialization logic.
@@ -51,6 +59,7 @@ namespace CarGame
             cameraNode.Position = new SCNVector3(0, 90, 0);
             cameraNode.EulerAngles = new SCNVector3(-pi/2, -pi/2, 0.0f);
             cameraNode.Camera.FieldOfView = 120;
+            cameraNode.Camera.UsesOrthographicProjection = false;
             gameScene.RootNode.AddChildNode(cameraNode);
         }
 
@@ -58,7 +67,7 @@ namespace CarGame
 
             tree = SCNPyramid.Create(23, 60, 23);
             treeNode = SCNNode.FromGeometry(tree);
-            treeNode.Position = new SCNVector3(0, 10.0f, 0);
+            treeNode.Position = new SCNVector3(0, 0, 0);
             treeNode.CastsShadow = true;
             gameScene.RootNode.AddChildNode(treeNode);
 
@@ -72,18 +81,21 @@ namespace CarGame
             light.LightType = SCNLightType.Omni;
             light.Color = UIColor.White;
             lightNode.Light = light;
-            lightNode.Position = new SCNVector3(-40, 40, 60);
+            lightNode.Position = new SCNVector3(-90, 40, 60);
             gameScene.RootNode.AddChildNode(lightNode);
 
             var directionLight = SCNNode.Create();
             directionLight.Light = SCNLight.Create();
+            directionLight.Light.LightType = SCNLightType.Directional;
             directionLight.Light.CastsShadow = true;
             directionLight.Light.ShadowMode = SCNShadowMode.Deferred;
             directionLight.Light.CategoryBitMask = (System.nuint)(-1);
             directionLight.Light.AutomaticallyAdjustsShadowProjection = true;
             directionLight.Light.MaximumShadowDistance = 50;
-            directionLight.Position = new SCNVector3(-40, 40, 60);
+            directionLight.Position = new SCNVector3(-90, 40, 60);
             directionLight.Rotation = new SCNVector4(x: -1, y: 0, z: 0, w: pi / 2);
+            directionLight.Light.ShadowColor = new UIColor(white: 0, alpha: 0.5f);
+
             gameScene.RootNode.AddChildNode(directionLight);
       
         }
@@ -108,12 +120,37 @@ namespace CarGame
 
 
         public void CreateUser() {
-            userShape = SCNBox.Create(8.0f, 7.0f, 15.0f, 0.005f);
-            user = SCNNode.FromGeometry(userShape);
-            user.Position = new SCNVector3(10, 4, -50);  // trying to create a car onto the top left position of the screen ||  coords are: (down/up floor, in/out floor, left/right floor)
+            userShape = SCNBox.Create(8.0f, 7.0f, 15.0f, 0.005f);  
+            user = SCNNode.FromGeometry(userShape);  // Creating a new node, user, that inherits the shape of the SCNBox, userShape
+            user.Position = new SCNVector3(10, 4, -50);  
+            user.PhysicsBody = SCNPhysicsBody.  // Trying to set a physics body for the user in order for us to give controls to it
             gameScene.RootNode.AddChildNode(user);
         }
 
+
+        public void CreateButtons(){
+            leftBtn = new UIButton(new CGRect(0.0, 0.0, View.Frame.Width / 2, View.Frame.Height));
+            leftBtn.BackgroundColor = new UIColor(0.5f, 0.5f);
+            leftBtn.AddTarget(ButtonEventHandler, UIControlEvent.TouchDown);
+
+            rightBtn = new UIButton(new CGRect(View.Frame.Width / 2, 0.0, View.Frame.Width / 2, View.Frame.Height));
+            rightBtn.BackgroundColor = new UIColor(43, 0, 0, 0.5f);
+            rightBtn.AddTarget(ButtonEventHandler, UIControlEvent.TouchDown);
+
+
+            View.AddSubview(rightBtn);
+            View.AddSubview(leftBtn);
+        }
+
+
+        public void ButtonEventHandler(object sender, EventArgs e)
+        {
+            if (sender == leftBtn) {
+                Console.WriteLine("LeftButton Touched");
+            } else if (sender == rightBtn) {
+                Console.WriteLine("RightButton Touched");
+            }
+        }
         public override void ViewDidLoad(){
             base.ViewDidLoad();
 
@@ -125,6 +162,7 @@ namespace CarGame
             CreateFloor();
             CreateLight();
             CreateUser();
+            CreateButtons();
             // create a new scene
 
         }
